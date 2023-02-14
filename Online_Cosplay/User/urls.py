7b0104e420fbf08fa59cae7fbe6c085e64aaf79b
zenosaika <zenosaika@gmail.com>
2023-02-14 09:39:29 +0700

    User can sign in now.

--- Online_Cosplay/User/urls.py
+++ Online_Cosplay/User/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
 ]
--- Online_Cosplay/User/views.py
+++ Online_Cosplay/User/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
 # Create your views here.
@@ -15,3 +16,18 @@ def register_request(request):
         messages.error(request, 'Invalid Information.')
     form = RegisterForm()
     return render(request, 'User/register.html', {'register_form':form})
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'Welcome {username}!')
+                return redirect('/')
+        messages.error(request, 'Invalid username or password.')
+    form = AuthenticationForm()
+    return render(request, 'User/login.html', {'login_form':form})
