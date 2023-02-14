83e9ae2b49460d121206e54bc0db00e02fdcfb75
zenosaika <zenosaika@gmail.com>
2023-02-14 09:58:07 +0700

    User can sign out now.

--- Online_Cosplay/User/urls.py
+++ Online_Cosplay/User/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register_request, name='register'),
     path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 ]
--- Online_Cosplay/User/views.py
+++ Online_Cosplay/User/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
@@ -31,3 +31,8 @@ def login_request(request):
         messages.error(request, 'Invalid username or password.')
     form = AuthenticationForm()
     return render(request, 'User/login.html', {'login_form':form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'Logout Successfully.')
+    return redirect('/')
